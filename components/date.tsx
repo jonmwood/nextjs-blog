6569ae2af18c8913185f3702489cae44df84f0f9
Jon <jonmwood@gmail.com>
2020-11-27 05:26:53 -0800

    convert pages to Typescript

--- components/date.tsx
+++ components/date.tsx
@@ -1,6 +1,9 @@
 import { parseISO, format } from 'date-fns'
 
-export default function Date({ dateString }) {
+export default function Date({ dateString }: { dateString: string }) {
+	console.log(dateString)
 	const date = parseISO(dateString)
+	console.log(date)
 	return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+	// return null
 }
